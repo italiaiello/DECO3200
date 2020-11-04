f09fc8c054b0b5bc43a7e75648c521ed5ab2b586
Andrew Aiello <andrewaiello7@gmail.com>
2020-11-04 14:52:28 +1100

    Added project files

---
+++ Eco Home/Assets/Scripts/ObjectInteraction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    void OnMouseOver()
+    {
+        print(gameObject.name);
+    }
+}
