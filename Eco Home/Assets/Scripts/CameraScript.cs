72fd0a6aeefcb4a7b9a7735a0947f030403f3108
Andrew Aiello <andrewaiello7@gmail.com>
2020-11-06 23:44:21 +1100

    Created basic laundry scene, removed WebXR library

---
+++ Eco Home/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Space)) {
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        }
+
+    }
+}
---
+++ Eco Home/Assets/Scripts/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tasks : MonoBehaviour
+{
+    public GameObject clothes, firstDetergent, secondDetergent, thirdDetergent;
+    public Image detergentTask, washTask, dryTask;
+
+    void Update() {
+        if (!firstDetergent || !secondDetergent || !thirdDetergent) {
+            detergentTask.GetComponent<Image>().color = Color.green;
+        }
+    }
+}
