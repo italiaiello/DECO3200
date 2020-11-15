c517a27c2753a6edaa1ee67695449f83ba5b1882
Andrew Aiello <andrewaiello7@gmail.com>
2020-11-15 16:13:26 +1100

    Got animations working

---
+++ Eco Home/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+
+    public Animator DoorPivot;
+
+    void OnTriggerEnter(Collider other) {
+        DoorPivot.SetBool("OpenDoor", true);
+        Debug.Log("Hello");
+
+    }
+
+    void OnTriggerExit(Collider other) {
+        Debug.Log("Hello");
+        DoorPivot.SetBool("OpenDoor", false);
+        if(!DestinationObject.GetDraggedItem().activeSelf) {
+            DoorPivot.SetBool("OpenDoor", false);
+        }
+
+    }
+}
